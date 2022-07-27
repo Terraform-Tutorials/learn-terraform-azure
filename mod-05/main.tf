3dcce57e71f3ee963944598ff98be63c9f96187a
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-07-27 09:17:49 -0300

    updates

--- mod-05/main.tf
+++ mod-05/main.tf
@@ -11,3 +11,9 @@ resource "azurerm_resource_group" "grupo-recurso" {
     data = formatdate("DD MMM YYYY hh:mm ZZZ", "2018-01-02T23:12:01Z")
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "vnettreinamentoazure"
+  location = "brazilsouth"
+  resource_group_name = "rg-terraform-mod5"
+}
