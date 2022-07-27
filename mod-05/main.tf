d2cbd37c8e715ed0a1203a15b3cfb9d743d21c4f
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-07-27 09:22:00 -0300

    upsates

--- mod-05/main.tf
+++ mod-05/main.tf
@@ -12,8 +12,18 @@ resource "azurerm_resource_group" "grupo-recurso" {
   }
 }
 
+variable "vnetips" {
+  type = list
+  default = [ "10.0.0.0/16" ]
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name = "vnettreinamentoazure"
   location = "brazilsouth"
   resource_group_name = "rg-terraform-mod5"
+  address_space = concat(var.vnetips, [ "192.168.0.0./16" ])
+}
+
+output "vnet-numeroips" {
+  value = lenght("${azure_virtual_network.vnet.address_space}")
 }
