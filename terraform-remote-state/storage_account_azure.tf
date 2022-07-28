5b923db54b6413a8ff281b8838fff31ff9e93443
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-07-28 07:41:32 -0300

    updates

--- terraform-remote-state/storage_account_azure.tf
+++ terraform-remote-state/storage_account_azure.tf
@@ -39,3 +39,21 @@ resource "azurerm_storage_account" "storagetf" {
   account_tier = "Hot"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "container" {
+  name = "terraform"
+  storage_account_name = azurerm_storage_account.storagetf.name
+  depends_on = [ azurerm_storage_account.storagetf ]
+}
+
+output "blobstorage-name" {
+  value = "azurerm_storage_account.storagetf.name"
+}
+
+output "blobstorage-chave-primaria" {
+  value = "azurerm_storage_account.storagetf.primary_access_key"
+}
+
+output "blobstorage-chave-secundaria" {
+  value = "azurerm_storage_account.storagetf.secondary_access_key"
+}
