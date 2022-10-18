5cc04f921dcf093776a690f64c5b98b4583a8f0f
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-10-18 10:47:36 -0300

    updates

---
+++ azure-terraform-labs/terraform-resource-group-azure/main.tf
@@ -0,0 +1,10 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Brazil South"
+}
